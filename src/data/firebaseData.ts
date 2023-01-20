a4f768fa4e537aee93211ae3955dc1a9f5b14648
David Ghibradze <96905686+Ghibrasoft@users.noreply.github.com>
2023-01-20 12:59:06 +0400

    Update firebaseData.ts

--- src/data/firebaseData.ts
+++ src/data/firebaseData.ts
@@ -3,15 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-export const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAtxh_wAPsl8ADFLefDB8l79sI8C8ld_YA",
-    authDomain: "superchat-b8348.firebaseapp.com",
-    projectId: "superchat-b8348",
-    storageBucket: "superchat-b8348.appspot.com",
-    messagingSenderId: "298169515045",
-    appId: "1:298169515045:web:bdc8fc921be027857253cc",
-    measurementId: "G-BP57SVGY41"
-});
+
 
 
 
